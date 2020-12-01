5d05aed24deaed2a382395b140e60c9b889cb92a
Peyton Jones <peytonj.dev@gmail.com>
2020-12-01 14:40:01 -0700

    add imports

---
+++ src/Movies.js
@@ -0,0 +1,22 @@
+import React from 'react'
+// import Card from 'Card'
+import './Movies.css'
+
+const Movies = (props) => {
+  return (
+    <h1>Hello</h1>
+  )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+export default Movies;
