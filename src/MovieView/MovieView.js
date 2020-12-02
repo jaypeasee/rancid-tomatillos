93a2010a166bb040aedcdc7962894a448e40a310
JP Carey <jpcarey4@gmail.com>
2020-12-02 16:42:38 -0700

    Style movie view

--- src/MovieView/MovieView.js
+++ src/MovieView/MovieView.js
@@ -7,13 +7,12 @@ class MovieView extends Component {
     }
 
     render() {
-        console.log(this.props)
         const {id, backdrop_path, title, average_rating, release_date} = this.props.currentMovie
         return (
-            <article id={id}>
-                <img src={backdrop_path} alt={title + " movie poster"}/>
-                <title>{title}</title>
-                <ul>
+            <article id={id} className="movie-page">
+                <img className="movie-backdrop" src={backdrop_path} alt={title + " movie poster"}/>
+                <h1>{title}</h1>
+                <ul className="movie-specs">
                     <li>Average Rating: {average_rating}</li>
                     <li>Release Date: {release_date}</li>
                 </ul>
