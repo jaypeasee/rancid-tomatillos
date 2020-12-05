7f8f14015194144f44962cd5455a267a063e025c
Peyton Jones <peytonj.dev@gmail.com>
2020-12-05 12:25:05 -0700

    add arai label

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -13,6 +13,7 @@ class Card extends Component {
     const {id, image, alt, showChosenMovie} = this.props
     return (
     <img className="movie-card"
+      aria-label={alt + " movie cover"}
       id={id} 
       src={image} 
       alt={alt + " movie cover"}
