f22ca7940783dd7e14e085b43f112f5bc6f462dc
Peyton Jones <peytonj.dev@gmail.com>
2020-12-05 12:26:23 -0700

    create imports for MovieView

---
+++ src/MovieView/MovieView.test.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { fireEvent, screen, render } from '@testing-library/react'
+// import Card from './Card.js'
+import '@testing-library/jest-dom'
+import MovieView from './MovieView.js'
+
+
+export default MovieView;
