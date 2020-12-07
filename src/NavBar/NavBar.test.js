1b5c3aed1c11affce5de0ba2c678b2013479e0e0
JP Carey <jpcarey4@gmail.com>
2020-12-07 16:25:59 -0700

    import fireEvent to pass click test on home button

--- src/NavBar/NavBar.test.js
+++ src/NavBar/NavBar.test.js
@@ -1,4 +1,4 @@
-import { screen, render } from '@testing-library/react'
+import { screen, render, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import NavBar from './NavBar.js'
 
@@ -6,25 +6,23 @@ import NavBar from './NavBar.js'
 describe('NavBar', () => {
   it('should have a title and subtitle', () => {
     render(<NavBar />)
-  
-    
     expect(screen.getByText('Welcome to Rancid Tomatillos!')).toBeInTheDocument()
     expect(screen.getByText('A page to view detailed movie reviews and their trailers')).toBeInTheDocument()
   })
 
-  // it('should return to home ', () => {
-  //   const mockReturnToHome = jest.fn()
+  it('should return to home ', () => {
+    const mockReturnToHome = jest.fn()
     
-  //   render(<NavBar
-  //     returnToHome={mockReturnToHome}
-  //     toggled={true}
-  //     />)
+    render(<NavBar
+      returnToHome={mockReturnToHome}
+      toggled={true}
+      />)
 
-  //   const homeBtn = screen.getByText('Home')
+    const homeBtn = screen.getByText('Home')
 
-  //   fireEvent.click(homeBtn);
+    fireEvent.click(homeBtn);
 
-  
-  //   expect(mockReturnToHome).toHaveBeenCalledWith()
-  // })
+    screen.debug()
+    expect(mockReturnToHome).toHaveBeenCalled()
+  })
 })
