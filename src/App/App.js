9dcbb798c97da1375d51fe312c106a6ae72d46b0
JP Carey <jpcarey4@gmail.com>
2020-12-09 20:35:39 -0700

    Test the Error

--- src/App/App.js
+++ src/App/App.js
@@ -46,7 +46,7 @@ class App extends Component {
             render={() => {
               return <Movies 
                 movies={this.state.movies}
-                />
+            />
             }}/>
           <Route 
             exact 
---
+++ src/Error/Error.test.js
@@ -0,0 +1,11 @@
+import {screen, render} from '@testing-library/react'
+import '@testing-library/jest-dom'
+import Error from './Error'
+
+describe('Error', () => {
+    it('should have a message', () => {
+        render(<Error />)
+
+        expect(screen.getByText('Oops! This page is not found.')).toBeInTheDocument();
+    })
+})
