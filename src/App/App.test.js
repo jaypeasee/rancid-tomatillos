02f19fa0a3ea7328c484d85fbfb4b6f106ab9e53
JP Carey <jpcarey4@gmail.com>
2020-12-09 17:21:21 -0700

    Add history pathname tests to app.

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -6,7 +6,6 @@ jest.mock('../apiCalls')
 import userEvent from '@testing-library/user-event'
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
-import { MemoryRouter } from "react-router-dom";
 
 describe('App', () => {
   let mockMovies;
@@ -28,6 +27,26 @@ describe('App', () => {
     expect(secMovieAltTxt).toBeInTheDocument()
   })
 
+  it('should load the home page by default', async () => {
+    render(<Router history={history}><App /></Router>);
+    expect(history.location.pathname).toBe('/')
+  })
+
+  it('should route to a specific movie page when that movie is clicked', async () => {
+    render(<Router history={history}><App /></Router>)
+    const allMovieSpecs = {movie: {id:694919,title:"Money Plane",poster_path:"https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",backdrop_path:"https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",release_date:"2020-09-29",overview:"hey hey hey hey" ,genres:["Action"],budget:0,revenue:0,runtime: 82, tagline: "",average_rating: 6.666666666666667}}
+    const videoSpecs = {videos:[{id:330,movie_id:694919,key:"aETz_dRDEys",site:"YouTube",type:"Trailer"}]}
+    
+    getMovieByID.mockResolvedValueOnce(allMovieSpecs);
+    getMovieTrailerByID.mockResolvedValueOnce(videoSpecs);
+
+    const firstMovieAltTxt = await waitFor(() => screen.getByAltText("Money Plane movie cover"))
+    
+    userEvent.click(firstMovieAltTxt);
+
+    expect(history.location.pathname).toBe('/movie-review/694919')
+  })
+
   it('should show chosen movie specs', async () => {
     render(<Router history={history}><App /></Router>)
     const allMovieSpecs = {movie: {id:694919,title:"Money Plane",poster_path:"https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",backdrop_path:"https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",release_date:"2020-09-29",overview:"hey hey hey hey" ,genres:["Action"],budget:0,revenue:0,runtime: 82, tagline: "",average_rating: 6.666666666666667}}
@@ -47,7 +66,7 @@ describe('App', () => {
     expect(movieSpecs).toBeInTheDocument()
   })
 
-  it('should show return to home button when viewing single movie specs', async () => {
+  it('should return back to home when home button is clicked', async () => {
     render(<Router history={history}><App /></Router>)
 
     const allMovieSpecs = {movie: {id:694919,title:"Money Plane",poster_path:"https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",backdrop_path:"https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",release_date:"2020-09-29",overview:"A professional thief with $40 million in debt and his family's life on the line must commit one final heist - rob a futuristic airborne casino filled with the world's most dangerous criminals.",genres:["Action"],budget:0,revenue:0,runtime: 82, tagline: "",average_rating: 6.666666666666667}}
@@ -62,31 +81,11 @@ describe('App', () => {
 
     let homeBtn = await waitFor(() => screen.getByRole("button")) 
 
-    expect(homeBtn).toBeInTheDocument()
+    userEvent.click(homeBtn);
+
+    expect(history.location.pathname).toBe('/')
+
   })
-
-  // it('should call a function when home button is clicked', async () => {
-  //   render(<Router history={history}><App /></Router>)
-
-  //   const allMovieSpecs = {movie: {id:694919,title:"Money Plane",poster_path:"https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",backdrop_path:"https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",release_date:"2020-09-29",overview:"A professional thief with $40 million in debt and his family's life on the line must commit one final heist - rob a futuristic airborne casino filled with the world's most dangerous criminals.",genres:["Action"],budget:0,revenue:0,runtime: 82, tagline: "",average_rating: 6.666666666666667}}
-  //   const videoSpecs = {videos:[{id:330,movie_id:694919,key:"aETz_dRDEys",site:"YouTube",type:"Trailer"}]}
-    
-  //   getMovieByID.mockResolvedValueOnce(allMovieSpecs);
-  //   getMovieTrailerByID.mockResolvedValueOnce(videoSpecs);
-
-  //   const firstMovieAltTxt = await waitFor(() => screen.getByAltText("Money Plane movie cover"))
-    
-  //   userEvent.click(firstMovieAltTxt);
-
-  //   let homeBtn = await waitFor(() => screen.getByRole("button")) 
-
-  //   userEvent.click(homeBtn)
-
-  //   const notChosenMovie = screen.getByAltText('Mulan movie cover')
-
-  //   expect(notChosenMovie).toBeInTheDocument();
-  //   expect(homeBtn).not.toBeInTheDocument();
-  // })
 //   it('should have new url when a card is clicked', async () => {
 //     render(
 //     <MemoryRouter initialEntries={['/']}
@@ -96,4 +95,4 @@ describe('App', () => {
 //         render=
 //     </MemoryRouter>)
 //   })
-// });
+});
