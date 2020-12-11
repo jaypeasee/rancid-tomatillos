ae5692860d45d5cdb5dbbb57c804ff78f4baa86a
Peyton Jones <peytonj.dev@gmail.com>
2020-12-11 10:01:39 -0700

    create if statement to support search bar function

--- src/Movies/Movies.js
+++ src/Movies/Movies.js
@@ -4,23 +4,37 @@ import './Movies.scss'
 
 
 const Movies = (props) => {
+  let movieCards = [];
   if (props.movies.length === 0) {
     return (
       <h1 className="loading-message">Loading...</h1>
     )
   }
-
-  const movieCards = props.movies.map(movie => {
-    return (
-      <Card
-        id={movie.id}
-        image={movie.poster_path}
-        title={movie.title}
-        key={movie.id}  
-        average_rating={movie.average_rating}
-      />
-    )
-  })
+  if (props.filteredMovies.length > 0) {
+     movieCards = props.filteredMovies.map(movie => {
+      return (
+        <Card
+          id={movie.id}
+          image={movie.poster_path}
+          title={movie.title}
+          key={movie.id}  
+          average_rating={movie.average_rating}
+        />
+      )
+    })
+  } else {
+    movieCards = props.movies.map(movie => {
+      return (
+        <Card
+          id={movie.id}
+          image={movie.poster_path}
+          title={movie.title}
+          key={movie.id}  
+          average_rating={movie.average_rating}
+        />
+      )
+    })
+  }
   return (
     <section className="movies-container">
       {movieCards}
