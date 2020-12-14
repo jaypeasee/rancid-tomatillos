f23f93d2a9651e953532c0b3cea03ae2ea5a9669
JP Carey <jpcarey4@gmail.com>
2020-12-14 14:37:17 -0700

    added basename to browser router, built deploy again

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 
-const router = <BrowserRouter><App /></BrowserRouter>
+const router = <BrowserRouter basename='/rancid-tomatillos'><App /></BrowserRouter>
 
 ReactDOM.render(router,
   document.getElementById('root'));
